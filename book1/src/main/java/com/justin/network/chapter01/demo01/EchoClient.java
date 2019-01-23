36bc3890fa71b5137bd0eb6ebecf510e5e747b7a
jun.hu <jun.hu@picahealth.com>
2019-01-23 19:13:27 +0800

    changes

--- book1/src/main/java/com/justin/network/chapter01/demo01/EchoClient.java
+++ book1/src/main/java/com/justin/network/chapter01/demo01/EchoClient.java
@@ -3,6 +3,15 @@ package com.justin.network.chapter01.demo01;
 import java.io.*;
 import java.net.Socket;
 
+/**
+ * PrintWriter ： 向标准输出设备输出流，用来输出各种类型的数据。PrintWriter，有一系列的print方法和println方法，有缓冲区功能，
+ * 需要及时关闭或者flush,可以写入基本类型（转换为字符串）、字符串和对象（使用toString方法）
+ * PrintWriter 简单的理解为向OutputStream中写入数据的工具类。
+ *
+ * InputStream：从外部进入内部的流（数据） 从InputStream中读取数据
+ * OutputStream：从内部发到外部的流（数据）往OutputStream中写入数据
+ *
+ */
 public class EchoClient {
     private String host = "localhost";
     private int port = 8000;
@@ -35,6 +44,7 @@ public class EchoClient {
             String msg = null;
 
             while((msg = localReader.readLine()) != null) {
+                // 将msg发送到服务端
                 printWriter.println(msg);
                 System.out.println(bufferedReader.readLine());
 
---
+++ book2/src/main/java/com/justin/network/App.java
@@ -0,0 +1,13 @@
+package com.justin.network;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
